5ffe62d9ff0f9545e7df69ab42f61d15bf934c02
Marco Schmid <marco@marcoschmid.me>
2019-04-25 21:51:46 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using k8s;
+
+namespace PodRecycler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = KubernetesClientConfiguration.InClusterConfig();
+            var client = new Kubernetes(config);
+            var cNs = config.Namespace;
+            if (config.Namespace == null)
+                cNs = "default";
+
+            var pods = (client.ListNamespacedPod(cNs))
+                .Items.Where(x => x.Metadata.Labels.ContainsKey("podrecycler"));
+
+            foreach (var n in pods)
+            {
+                if (n.Metadata.Labels["podrecycler"] == "yes")
+                {
+                    Console.WriteLine($"deleting {n.Metadata.Name}");
+                    client.DeleteNamespacedPod(n.Metadata.Name, cNs);
+                }
+            }
+        }
+    }
+}
